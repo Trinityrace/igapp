b7f81c91d4cefe4ec6f6055470bf28882940198e
Trinity John <trinityrtime@gmail.com>
2020-06-11 17:25:23 +0300

    forms empty and views linked

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
+from .forms import *
 from django.http  import HttpResponse, Http404, HttpResponseRedirect
 import datetime as dt
+
 # Create your views here.
 
 def signup(request):
